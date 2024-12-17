615ebaddf47d7be7007564d57b29b4ec1f1a5a94
Aditt5 <9gadityarahman@gmail.com>
2024-12-17 21:42:16 +0800

    Hutao

---
+++ setting/settings.js
@@ -0,0 +1,135 @@
+//SCRIPT INI DI GRATISKAN, JANGAN SAMPAI ADA YG MENJUAL © CAYWZZ
+//BASE BY DITSS
+require("../all/module.js")
+
+//==setting store
+//========== Setting Owner ==========//
+global.owner = "6282154770530"
+global.owner2 = "6282154770530@s.whatsapp.net"
+global.nomorbot = '6281250432051' // nomor bot nya
+
+global.idsaluran = "120363357312070270@newsletter"
+global.idch = "120363357312070270@newsletter"
+//ganti id ch kalian "kalo gaada yudah jangan di ubah🗿"
+global.testi = "https://whatsapp.com/channel/0029Vau1badL7UVPMi0WMk0A"
+//saluran testi kamuu
+//======= SubDomains ============//
+global.saluran = 'https://whatsapp.com/channel/0029Vau1badL7UVPMi0WMk0A'
+//pasang link ch kalian
+
+global.gc = 'https://whatsapp.com/channel/0029Vau1badL7UVPMi0WMk0A'
+//ubah link gc kalian ke tinyurl
+
+global.group = 't.me/caywzzneh'
+global.namastore = "dits marketplace"
+//nama store
+
+global.namaowner = "CaywZz"
+// nama owner
+
+global.namabot = "Hutao"
+//nama bot
+
+global.namabot2 = "Hutao Only"
+
+global.version = "V1.8"
+//jangan di ubah
+
+global.linkyt = 'https://www.youtube.com/' 
+global.mail = 'Hutao@gmail.com' 
+global.token_do = "-"
+//==setting stiker 
+global.foother = "© Hutao the cpanel"
+global.packname = "Sticker By"
+global.author = "\nCreate by bot Hutao\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.L = "© powered by caywzz"
+//==setting apikey
+global.api = "cs_2AtCW"
+//========== Setting Event ==========//
+global.autoread = true
+global.anticall = false
+global.autoreadsw = true
+
+global.game = true // guna agar command game bisa dimatikan/diaktifkan
+//========== Setting Foto ===========//
+global.imgreply = "https://files.catbox.moe/ueipwo.jpg"
+global.thumb = "https://files.catbox.moe/v29m2w.jpg"
+global.thumb2 = "https://files.catbox.moe/c2acgi.jpg"
+global.thumb3 = "https://files.catbox.moe/v29m2w.jpg"
+global.thumb4 = "https://files.catbox.moe/v29m2w.jpg"
+//global.imgmenu = fs.readFileSync("./media/Menu.jpg")
+//==setting sever private==//
+global.domainn = "-"
+global.apikeyy = "-"
+global.capikeyy = "-"
+
+//========== Setting Panell ==========//
+global.egg = '15' // id eggs yang dipakai kalo id nya 5 biarin aja ini jangan di ubah
+global.loc = '1' // id location
+global.crown = "2823" // jangan di ubah
+global.limitawal = 5
+
+global.domain = '-' // Isi Domain Lu
+global.apikey = '-' // Isi Apikey Plta Lu
+global.capikey = '-' // Isi Apikey Pltc Lu
+
+//========= Setting Payment =========//
+global.dana = ""
+global.gopay = ""
+global.ovo = ""
+global.qris = "https://files.catbox.moe/vgfxyi.jpg"
+// SETTING GAME
+global.gamewaktu = 60 // Game waktu
+global.bmin = 1000 // Balance minimal 
+global.bmax = 3000 // Balance maksimal
+global.limit = 15 // Set limit
+global.limitawal = {
+premium: "Infinity",
+free: 8000,
+monayawal: 1000
+}
+
+//===setting game===
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+//========= Setting Message =========//
+
+global.msg = {
+    done: 'done✓',
+    wait: '⏳memproses . . .',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    adminbot: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: '⏳ Sedang Di Proses !',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !\n\n🎯 *Premium Cuma 10k Permanen* 😋',
+    error: 'fitur sedang error !',
+    prem: '🚫 fitur khusus premium!\n\n♨️ ',
+    developer: 'Command Ini Hanya Untuk Developer Bot!'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
